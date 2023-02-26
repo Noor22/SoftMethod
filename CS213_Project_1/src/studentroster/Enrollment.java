58b1fe631fada3203eb4ea0ad401c14563b1e696
GmtDT <dylanturnerfly@gmail.com>
2023-02-26 00:22:05 -0500

    Implemented the methods in the enrollment class

--- CS213_Project_1/src/studentroster/Enrollment.java
+++ CS213_Project_1/src/studentroster/Enrollment.java
@@ -4,22 +4,56 @@ public class Enrollment {
     private EnrollStudent[] enrollStudents;
     private int size;
 
-    public Enrollment(){
-        this.enrollStudents = new EnrollStudent[Constant.INITIAL_SIZE.getValue()];
+    public Enrollment() {
+        this.enrollStudents = new EnrollStudent[Constant.ENROLLMENT_INITIAL.getValue()];
         this.size = 0;
     }
 
-    public void add(EnrollStudent enrollStudent){ //add to the end of array. move the last one in the array to replace the deletting index position
-
+    public void add(EnrollStudent enrollStudent) {
+        if(this.size == enrollStudents.length){
+            int newLength = this.size + 1;
+            EnrollStudent[] tempEnroll = new EnrollStudent[newLength];
+            for(int i = 0; i < this.size; i++) {
+                tempEnroll[i] = this.enrollStudents[i];
+            }
+            tempEnroll[size] = enrollStudent;
+            this.enrollStudents = tempEnroll;
+            this.size++;
+            return;
+        }
+        enrollStudents[size] = enrollStudent;
     }
 
-    public void remove(EnrollStudent enrollStudent){
-
+    public void remove(EnrollStudent enrollStudent) {
+        int removePosition = find(enrollStudent);
+        if(removePosition != Constant.NOT_FOUND.getValue()){
+            enrollStudents[removePosition] = enrollStudent;
+            enrollStudents[this.size] = null;
+            this.size--;
+        }
     }
 
-    public boolean contains(EnrollStudent enrollStudent){
-        return true;
+    public boolean contains(EnrollStudent enrollStudent) {
+        for(int i = 0; i < this.size; i++) {
+            if(enrollStudents[i].equals(enrollStudent)){
+                return true;
+            }
+        }
+        return false;
     }
 
-    public void print() {} //print the array as is without sorting
+    public int find(EnrollStudent enrollStudent) {
+        for(int i = 0; i < this.size; i++) {
+            if(enrollStudents[i].equals(enrollStudent)){
+                return i;
+            }
+        }
+        return Constant.NOT_FOUND.getValue();
+    }
+
+    public void print() { //print the array as is without sorting
+        for(int i = 0; i < this.size; i++){
+            System.out.println(enrollStudents[i].toString());
+        }
+    }
 }
