af7002f2a1694041dbc15bbebb0c78c3676adc58
Noor22 <noor@nadmm.com>
2023-02-26 02:26:33 -0500

    added more methods to student classes

--- CS213_Project_1/src/studentroster/International.java
+++ CS213_Project_1/src/studentroster/International.java
@@ -6,4 +6,19 @@ public class International extends NonResident {
     public International(Profile profile, String major,int creditCompleted){
         super(profile,  major, creditCompleted);
     }
+    public double tuitionDue(int creditsEnrolled){
+        double tuition;
+        if(isStudyAbroad) {
+             tuition = super.tuitionDue(creditsEnrolled); // uses nonresident tuition as a base
+                tuition += TuitionValues.HEALTH_INSURANCE_FEE.getValue();
+        }else{
+             tuition = TuitionValues.FULL_UNIVERSITY_FEE.getValue() + TuitionValues.HEALTH_INSURANCE_FEE.getValue();
+        }
+        return tuition;
+    }
+
+    @Override
+    public boolean isResident() {
+        return super.isResident();
+    }
 }
